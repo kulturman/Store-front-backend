c9c407bf4b64443a2c99e6e2e717b17203a88223
kulturman <arnaudbakyono@gmail.com>
2023-01-07 17:20:03 +0000

    Add docker-compose and dependencies

--- server.ts
+++ server.ts
@@ -3,5 +3,6 @@ import express, { Express } from 'express';
 const app: Express = express();
 const port = 3000;
 
+app.use(express.json());
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
